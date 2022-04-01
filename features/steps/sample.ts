ca1a3a0c613b43120a78c1759d9013760a05cf0e
horeanu radu <horeanu.raducatalin.ext@eulerhermes.com>
2022-04-01 17:35:47 +0200

    add solution

--- features/steps/sample.ts
+++ features/steps/sample.ts
@@ -16,8 +16,48 @@ Then('the score is {int}', function (int) {
   expect(this.score).to.equal(int)
 })
 
+function isSpare(frame) {
+  return frame[1] === '/'
+}
+
+function isStrike(frame) {
+  return frame === 'X'
+}
+
+function calculateScoreFromFrame(frame) {
+  if (!frame) {
+    return [0, 0]
+  } else if (isSpare(frame)) {
+    return [parseInt(frame[0]), 10 - parseInt(frame[0])]
+  } else if (isStrike(frame)) {
+    return [10, 0]
+  } else {
+    const try1 = parseInt(frame[0])
+    const try2 = parseInt(frame[1])
+    return [isNaN(try1) ? 0 : try1, isNaN(try2) ? 0 : try2]
+  }
+}
+
 function calculeScore(frames) {
-  const numbers = frames.match(/[0-9]/g) || []
-  const score = numbers.reduce((acc, curr) => acc + parseInt(curr), 0)
+  let score = 0
+  const splitedFrames = frames.split(' ')
+  splitedFrames.forEach((frame, frameIndex) => {
+    const frameScore = calculateScoreFromFrame(frame)
+    const nextFrameScore = calculateScoreFromFrame(splitedFrames[frameIndex + 1])
+    // console.log('-------------------------------')
+    // console.log('frameIndex => ', frameIndex)
+    // console.log('score =>', score)
+    // console.log('frame => ', frame)
+    // console.log('frameScore => ', frameScore)
+    // console.log('nextFrameScore =>', nextFrameScore)
+    // console.log('frames[frameIndex] =>', splitedFrames[frameIndex + 1])
+    if (frameScore[0] + frameScore[1] < 10) {
+      score += frameScore[0] + frameScore[1]
+    } else if (frameScore[0] === 10) {
+      score += frameScore[0] + frameScore[1] + nextFrameScore[0] + nextFrameScore[1]
+    } else if (frameScore[0] + frameScore[1] === 10) {
+      score += frameScore[0] + frameScore[1] + nextFrameScore[0]
+    }
+  })
   return score
 }
